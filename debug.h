3ee474ece9d526195fe213cd1761483d8cab069d
Saij <christoph@christophfriedrich.de>
2016-11-25 00:07:22 +0100

    RotEnc

---
+++ debug.h
@@ -0,0 +1,21 @@
+#define DEBUG
+#ifdef DEBUG
+
+#include <Arduino.h>
+#include <stdarg.h>
+#define PRINTF_BUF 80
+
+void setupDebug();
+
+void debugPrintf(const char *format, ...);
+
+#ifdef F // check to see if F() macro is available
+void debugPrintf(const __FlashStringHelper *format, ...);
+#endif // F
+
+#else // DEBUG
+
+#define setupDebug()
+#define debugPrintf(format, ...)
+
+#endif // DEBUG
